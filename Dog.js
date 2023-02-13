9f35011883bb03219aeccd59cb197083640090c7
2imMinh <nguyentuanminh.cn@gmail.com>
2023-02-14 03:04:17 +0700

    commit

--- Dog.js
+++ Dog.js
@@ -8,3 +8,25 @@ Dog.prototype.eat = function () {
 }
 
 module.exports = Dog;
+
+function groupByName(arr) {
+    let result = [];
+    let map = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        let name = arr[i].name;
+        let count = arr[i].count;
+
+        if (!map.has(name)) {
+            map.set(name, { name: name, count: 0 });
+            result.push(map.get(name));
+        }
+        map.get(name).count += count;
+    }
+
+    return result;
+}
+
+let arr = groupByName(e);
+
+console.log(arr);
